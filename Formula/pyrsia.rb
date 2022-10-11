2802bd054cacdb19b29633e3b9eb9be251074d12
Manas Das <manasd@jfrog.com>
2022-10-11 15:49:18 -0700

    Pyrsia homebrew installer

--- Formula/pyrsia.rb
+++ Formula/pyrsia.rb
@@ -2,12 +2,13 @@ class Pyrsia < Formula
   desc "Open source software that helps protect the open source supply chain"
   homepage "https://pyrsia.io/"
   url "https://brewrepo.pyrsia.io/pyrsia-0.2.0.tar.gz"
-  sha256 "7974164c0557823e91878f17af2e9adb2a9ff596347413fe09c8105f16ec7656"
+  sha256 "993e536ea7cd41b5d5b3dddeebb3333c29774255cf9d37c64d5da9e609ecfec4"
   license "Apache-2.0"
   version "0.2.0"
 
   def install
     bin.install "pyrsia"
+    bin.install "pyrsia_node"
   end
 
   test do
